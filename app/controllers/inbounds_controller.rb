3403cd5a7f0d0149f1b45b26b573d9b3c9cde0e6
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-27 13:08:50 -0800

    debug message with .read added to body

--- app/controllers/inbounds_controller.rb
+++ app/controllers/inbounds_controller.rb
@@ -2,6 +2,12 @@ class InboundsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
+    puts "Welcome to the CREATE method"
+    puts "Lets parse that json"
+    response = JSON.parse(response)
+    puts "Here, have a response object body:"
+    puts response.body.read
+    puts "==================================="
     inbound = Inbound.new(response)
     Ticket.find_and_update!(inbound)
     render nothing: true
--- app/models/inbound.rb
+++ app/models/inbound.rb
@@ -10,6 +10,20 @@ private
   def ticket_values_from_json(response)
     puts "AT LEAST I MADE IT INSIDE!!!!!!"
     puts "==============================="
+    puts "Response: #{response}"
+    puts "==============================="
+    puts "Response Body: #{response.body()}"
+    puts "==============================="
+    puts "Response Cookies: #{response.cookies()}"
+    puts "==============================="
+    puts "Does it Respond to body?: #{response.respond_to?(body)}"
+    puts "==============================="
+    puts "Response Status: #{response.status}"
+    puts "==============================="
+    puts "Split the body into array:"
+    puts response.body.scan(/\[(.*?)\]/m)
+    puts "==============================="
+
     response.body.scan(/\[(.*?)\]/m).flatten.map do |val|
       val[3..-1]
       puts "THE VALUE OF THIS IS: #{val}"
