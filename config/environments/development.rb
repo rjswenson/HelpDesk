e06149e130626cc7e04f26d30df23b84bcd2eecc
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-26 16:04:29 -0800

    setup for letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,17 @@
 Helpdesk::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: "helpdesk-ticketter.herokuapp.com" }
+  # config.action_mailer.smtp_settings = {
+  #   :port           => '25',
+  #   :address        => ENV['POSTMARK_SMTP_SERVER'],
+  #   :user_name      => ENV['POSTMARK_API_KEY'],
+  #   :password       => ENV['POSTMARK_API_KEY'],
+  #   :domain         => 'helpdesk-ticketter.heroku.com',
+  #   :authentication => :plain,
+  # }
+  config.action_mailer.delivery_method = :letter_opener
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,11 +25,9 @@ Helpdesk::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.raise_delivery_errors = false
 
   # specify what domain to use for mailer URLs
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
