160b4fc139f53cd386e8723adbf21548f7f4f745
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-27 15:35:50 -0800

    break apart logic

--- app/models/inbound.rb
+++ app/models/inbound.rb
@@ -8,10 +8,17 @@ class Inbound
 
 private
   def ticket_values_from_json(response_obj)
+    puts "=============================="
+    puts response_obj.inspect
+    puts "=============================="
     response_obj = response_obj["TextBody"]
     puts "Response Object now is:"
     puts response_obj
-    response_obj.scan(/\[(.*?)\]/m).map do |val|
+    derp = response_obj.scan(/\[(.*?)\]/m)
+    puts "This should be an array of the 3 objs:"
+    puts derp.inspect
+    puts "=================================="
+    derp.map do |val|
       puts "Value is first: #{val}"
       val[3..-1]
       puts "Now it IS: #{val}"
