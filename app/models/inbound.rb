18b610503072d8cf8a888f750bea336e9d6d457c
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-26 16:15:05 -0800

    parse json with regex into array

---
+++ app/models/inbound.rb
@@ -0,0 +1,15 @@
+class Inbound
+  attr_accessor :reply, :ticket_id, :completed
+
+  def initialize(response)
+    @ticket_id, @reply, @completed =
+      ticket_values_from_json(response)
+  end
+
+private
+  def ticket_values_from_json(response)
+    response.body.scan(/\[(.*?)\]/m).flatten.map do |val|
+      val[3..-1]
+    end
+  end
+end
