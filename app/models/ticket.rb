c9d85de8d9a317bc65054c8b7f818a5a4d0fc41e
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-26 16:02:49 -0800

    class method to update ticket on inbound

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,11 @@ class Ticket < ActiveRecord::Base
   validates :email, presence: true
   validates :complainant_name, presence: true
   validates :problem, presence: true
+
+  def self.find_and_update!(inbound)
+    ticket = Ticket.find(inbound.ticket_id)
+    ticket.response += inbound.reply
+    ticket.completed = inbound.completed.chomp.to_bool
+    ticket.save!
+  end
 end
