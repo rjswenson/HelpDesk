db4600d039ae8e09e6af79783cd2506e9bf643dd
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-26 17:36:51 -0800

    debug puts

--- app/controllers/inbounds_controller.rb
+++ app/controllers/inbounds_controller.rb
@@ -2,7 +2,7 @@ class InboundsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Ticket.find_and_update! Inbound.new(response)
+    Ticket.find_and_update!(Inbound.new(response))
     render nothing: true
   end
 end
--- app/models/inbound.rb
+++ app/models/inbound.rb
@@ -10,7 +10,8 @@ private
   def ticket_values_from_json(response)
     response.body.scan(/\[(.*?)\]/m).flatten.map do |val|
       val[3..-1]
-      puts val
+      puts "THE VALUE OF THIS IS: #{val}"
+      puts "==========================="
     end
   end
 end
