25ee1809f7ae83cb1222716b9a2671de2414cfa9
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-27 16:36:42 -0800

    scan ["TextBody"] only

--- app/models/inbound.rb
+++ app/models/inbound.rb
@@ -8,7 +8,7 @@ class Inbound
 
 private
   def ticket_values_from_json(response_obj)
-    response_obj.scan(/\[(.*?)\]/m).flatten.map do |val|
+    response_obj["TextBody"].scan(/\[(.*?)\]/m).flatten.map do |val|
       val[4..-1]
     end
   end
